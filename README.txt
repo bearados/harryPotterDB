c504deff823146e63ca28012965832220ebf5b1b
Jessica Adams <adamsjes@flip3.engr.oregonstate.edu>
2019-03-06 21:49:13 -0800

    added readme, updated findStudent view and getStudent.js

---
+++ README.txt
@@ -0,0 +1,7 @@
+HarryPotter DB
+
+To run:
+$ node hpdp.js [port]
+
+To view website go to: http://flip3.engr.oregonstate.edu:[port]
+
--- getStudent.js
+++ getStudent.js
@@ -1,29 +1,35 @@
 module.exports = function () {
     var express = require('express');
     var router = express.Router();
+    
+    /*function delPerson(res, mysql, context, id, complete) {
+        var sql = "SELECT * FROM student WHERE ID = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.person = results[0];
+            complete();
+        });
+    }*/
 
 
     router.get('/', function (req, res, next){
         var context = {};
-        var mysql = req.app.get('mysql');
-        var query = "select * from student";
-        mysql.pool.query(query, function (err, result) {
-            if (err) {
-                next(err);
-                return;
-            }
-            context.student = result;
-            
-            res.render('findStudent', context);
-        });
+        //var mysql = req.app.get('mysql');
+        //var query = "select * from student";
+        
+        res.render('findStudent', context);
     });
 
-    router.post('/findStudent', function (req, res, next) {
+    router.post('/', function (req, res, next) {
         console.log("in post: " + req.body.FirstName);
         var context = {};
         var query1 = "select * from student where FirstName = ? AND LastName = ?";
         var test = "select * from teacher";
-        var inserts = [req.body.FirstName, req.body.LastName]
+        var inserts = [req.body.firstname, req.body.lastname];
         var mysql = req.app.get('mysql');
         mysql.pool.query(query1, inserts, function (err, result) {
             if (err) {
@@ -40,8 +46,7 @@ module.exports = function () {
         callbackCount = 0;
         var context = {};
         var mysql = req.app.get('mysql');
-        var query1 = "select * from student where ID = ?";
-        var inserts = id;
+        
         mysql.pool.query(query1, inserts, function (err, result) {
             if (err) {
                 next(err);
