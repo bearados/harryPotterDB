e8829ec52582b7f96b6c250054676778d189e377
Jessica Adams <adamsjes@flip3.engr.oregonstate.edu>
2019-03-19 20:06:43 -0700

    fixed delete teacher on findTeacher, and addTeacher, add select house on add teacher

--- addTeacher.js
+++ addTeacher.js
@@ -1,18 +1,43 @@
 module.exports = function () {
     var express = require('express');
     var router = express.Router();
-    
+
+    function getHouseList(res, mysql, context, complete) {
+        mysql.pool.query("SELECT ID, HouseName FROM house", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.house = results;
+            complete();
+        });
+    }
+
+    function getTeachList(res, mysql, context, complete) {
+        var query1 = "SELECT teacher.ID, teacher.FirstName, teacher.LastName, house.HouseName FROM teacher left join house on teacher.HeadofHouse = house.ID";
+        mysql.pool.query(query1, function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.teacher = results;
+            complete();
+        });
+    }
+
     router.get('/', function (req, res, next) {
         var context = {};
         var mysql = req.app.get('mysql');
-        mysql.pool.query("SELECT teacher.FirstName, teacher.LastName, house.HouseName FROM teacher left join house on teacher.HeadofHouse = house.ID", function (error, results, fields) {
-            if (error) {
-                next(error);
-                return;
+        var callbackCount = 0;
+        getHouseList(res, mysql, context, complete);
+        getTeachList(res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 2) {
+                res.render('addTeacher', context);
             }
-            context.teacher = results;
-            res.render('addTeacher', context);
-        });
+
+        }
 
     });
 
